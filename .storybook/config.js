9cdeb47bcc51a2756efd34009e54a7822b97789f
christina <christina.inching.triceps@gmail.com>
2020-03-03 02:15:51 +0900

    add ts setting. but, not correct

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from '@storybook/html'
 
 function loadStories() {
-  const req = require.context('../src', true, /\.stories\.js$/)
+  const req = require.context('../src', true, /\.stories\.ts$/)
   req.keys().forEach(filename => req(filename))
 }
 
--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,14 @@ const path = require('path');
 module.exports = {
   webpackFinal: async config => {
 
+    // add ts setting
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [{
+        loader: require.resolve('ts-loader'),
+      }],
+    });
+
     // add sass setting
     config.module.rules.push({
       test: /\.scss$/,
@@ -17,6 +25,7 @@ module.exports = {
       include: path.resolve(__dirname, '../'),
     });
 
+    config.resolve.extensions.push('.ts', '.tsx', '.html');
     return config;
   },
 };
